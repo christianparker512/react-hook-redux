c46aa04a4e98fe80e58de18f554a3c0b20ae5dca
Christian Parker <christianparker512@gmail.com>
2021-07-15 15:02:20 -0500

    trial

---
+++ src/script.js
@@ -0,0 +1,6 @@
+import APP from './component1.js'
+import {banana, orange} from './component2.js'
+
+console.log('export default', APP)
+console.log('export default', banana)
+console.log('export default',orange)
